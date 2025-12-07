fe69225238f9c9cc6b97ff6074c29c8a1361cfc5
wukang <893276941@qq.com>
2025-12-07 13:42:17 +0000

    chore: Update Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 【这里是关键】
+  base: '/English-Learning-Web/', // ⚠️ 注意：这里必须换成你的仓库名，否则部署后是白屏！
+  build: {
+    outDir: 'docs', // 告诉 Vite 把构建结果输出到 docs 目录
+  }
 })
